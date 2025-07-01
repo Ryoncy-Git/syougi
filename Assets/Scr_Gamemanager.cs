dc1a8efa131878d06ed202f5c2be8d195cb9fca8
Ryoncy-Git <ryonryon5329@i.softbank.jp>
2025-07-01 10:15:43 +0900

    編集
    大まかなアウトラインを記載
    クラスの分類とか駒野の類とかはどうしようかまだ悩み中

--- Assets/Scr_Gamemanager.cs
+++ Assets/Scr_Gamemanager.cs
@@ -2,27 +2,35 @@ using UnityEngine;
 
 public class Scr_GameManager : MonoBehaviour
 {
-    private Scr_Class_Piece selectedPiece;
+    private Scr_Piece selectedPiece;
     private Transform[,] grid = new Transform[9, 9];
+    bool is1PTurn = true;
 
-    public void SelectPiece(Scr_Class_Piece piece)
+    public void SelectPiece(Scr_Piece piece)
     {
-        if (selectedPiece != null)
+        if (is1PTurn)
         {
-            selectedPiece.Deselect();
+            if (selectedPiece != null)
+            {
+                selectedPiece.Deselect();
+            }
+
+            selectedPiece = piece;
+            selectedPiece.Select();
         }
-
-        selectedPiece = piece;
-        selectedPiece.Select();
     }
-
-    public void grid_SetTransform(Transform piece, int x, int y)
+    public void grid_setTransform(Transform piece, int x, int y)
     {
         grid[x, y] = piece;
     }
 
-    public Transform grid_GetTransform(int x, int y)
+    public Transform grid_getTransform(int x, int y)
     {
         return grid[x, y];
+    }
+
+    public void change_turn()
+    {
+        is1PTurn = !is1PTurn;
     }
 }
--- Assets/Scr_Piece.cs
+++ Assets/Scr_Piece.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
 
-public class Scr_Class_Piece : MonoBehaviour
+public class Scr_Piece : MonoBehaviour
 {
     //全駒で共通の特徴を書く
-    public bool isSelected = false;
+    bool isSelected = false;
     Scr_GameManager gameManager;
 
     void Start()
+    {
+        init();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void init()
     {
         gameManager = GameObject.Find("Obj_GameManager").GetComponent<Scr_GameManager>();
     }
@@ -21,15 +31,26 @@ public class Scr_Class_Piece : MonoBehaviour
         Debug.Log("選択しました");
         // ハイライト表示などがあればここで
         // show_path()
-        // if(path != null)
-        // {
-            // transform.position = (行先)
-        // }
+        // movement
     }
 
     public void Deselect()
     {
         isSelected = false;
         // ハイライト解除など
+    }
+
+    void show_path()
+    {
+        //showing path
+    }
+
+    void movement()
+    {
+        // transform = (行先);
+        // if(catch)
+        // {
+        //     catch(行先の駒)
+        // }
     }
 }
